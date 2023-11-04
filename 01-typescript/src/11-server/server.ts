56b9c4c859bd492ba7d9947857eb46a2e40e4dce
Andrew <apwicker@gmail.com>
2023-11-04 14:48:56 -0700

    Add dotenv package to project and update .gitignore
    - Added dotenv package to the project.
    - Updated the .gitignore file to exclude 01-typescript/.env from version control.

--- 01-typescript/src/11-server/server.ts
+++ 01-typescript/src/11-server/server.ts
@@ -5,6 +5,7 @@ import apiRouter from '..routes/api';
 import mongoApi from '../routes/mongoApi';
 import mongoose from 'mongoose';
 import { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 const app: Express = express();
 const port: number = parseInt(process.env.PORT as string, 10) || 3000;
